901c19d626dc8aea09e5473f699c0f1d027ed7f6
Ollie Jones <oathompsonjones@gmail.com>
2024-11-01 22:00:59 +0000

    Improve some readability

--- src/components/splitView.tsx
+++ src/components/splitView.tsx
@@ -19,10 +19,12 @@ export function SplitView({ className, children, vertical }: {
     vertical?: boolean;
 }): ReactNode {
     const orientation = vertical ?? false ? "vertical" : "horizontal";
-    const splitViewClass = `${styles.container} ${styles[orientation]}`;
+    const splitViewClass = `${styles.container} ${styles[orientation]} ${className}`;
+    const container = useRef<HTMLDivElement>(null!);
 
     const [canDrag, setCanDrag] = useState(false);
-    const container = useRef<HTMLDivElement>(null!);
+    const enableDrag = (): void => setCanDrag(true);
+    const disableDrag = (): void => setCanDrag(false);
     const handleDrag = (event: MouseEvent<HTMLDivElement>): void => {
         if (canDrag) {
             const firstSection = container.current.children[0] as HTMLDivElement;
@@ -43,14 +45,9 @@ export function SplitView({ className, children, vertical }: {
     };
 
     return (
-        <div
-            className={`${splitViewClass} ${className}`}
-            ref={container}
-            onMouseMove={handleDrag}
-            onMouseLeave={() => setCanDrag(false)}
-        >
+        <div className={splitViewClass} ref={container} onMouseMove={handleDrag} onMouseLeave={disableDrag}>
             {children[0]}
-            <div className={styles.splitter} onMouseDown={() => setCanDrag(true)} onMouseUp={() => setCanDrag(false)}>
+            <div className={styles.splitter} onMouseDown={enableDrag} onMouseUp={disableDrag}>
                 <DragHandleRounded className={styles.dragIcon!} />
             </div>
             {children[1]}
