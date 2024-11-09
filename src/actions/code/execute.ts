48a0a7512a5203577af19f0d96283c6ca80a94c4
Ollie Jones <oathompsonjones@gmail.com>
2024-11-09 21:38:32 +0000

    Run the haskell code within a docker container

--- src/actions/code/execute.ts
+++ src/actions/code/execute.ts
@@ -6,21 +6,39 @@ import { promisify } from "util";
 const exec = promisify(execSync);
 
 /**
+ * Escapes special characters in a string.
+ * @param str - The string to escape.
+ * @returns The escaped string.
+ */
+function escapeChars(str: string): string {
+    let escapedStr = str;
+    const replacements: Array<[RegExp, string]> = [
+        [/'/g, "\\'"],
+        [/"/g, "\\\""],
+    ];
+
+    for (const [regex, replacement] of replacements)
+        escapedStr = escapedStr.replace(regex, replacement);
+
+    return escapedStr;
+}
+
+/**
  * Executes Haskell code.
  * @param code - The code to execute.
  * @returns The output of the code.
  */
 export async function execute(code: string): Promise<string> {
     try {
-        return (await exec(`echo '${code.replace(/'/g, "\\'")}' | runghc`)).stdout;
+        const cpuLimit = 0.5;
+        const dockerCmd = `docker run --rm -m 128m --cpus=${cpuLimit} haskell:latest`;
+        const bashCmd = `bash -c "echo '${escapeChars(code)}' > /tmp/script.hs && runghc /tmp/script.hs"`;
+
+        return (await exec(`${dockerCmd} ${bashCmd}`)).stdout;
     } catch (err) {
-        if (typeof err === "object" && err !== null && "stderr" in err) {
-            return String(err.stderr)
-                .trim()
-                .split("\n")
-                .slice(1)
-                .join("\n");
-        }
+        if (typeof err === "object" && err !== null && "stderr" in err)
+            return String(err.stderr);
+        // .trim().split("\n").slice(1).join("\n");
 
         return `Error: ${err instanceof Error ? err.message : String(err)}`;
     }
