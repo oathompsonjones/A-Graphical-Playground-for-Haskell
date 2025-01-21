3a05e145034e51f3f101e5df73652edb5cb7fd00
Ollie Jones <oathompsonjones@gmail.com>
2025-01-21 20:51:03 +0000

    Add a function to set the colour of individual pixels, and chance appendShape to an operator

--- public/lib/Lib.hs
+++ public/lib/Lib.hs
@@ -1,21 +1,25 @@
 module Lib (
     module Color,
-    module Point,
+    module Maths,
     module Shape,
+    Canvas (),
     render,
     createCanvas,
     background,
-    appendShape,
-    Canvas (),
+    (<<<),
+    setPixel,
 ) where
 
 import Color
-import Point
+import Maths
 import Shape
 
+-- Data structure to represent a canvas
 data Canvas = Canvas {width :: Int, height :: Int, backgroundColor :: Color, shapes :: [Shape]}
 
+-- Convert a canvas to a JSON string
 instance Show Canvas where
+    show :: Canvas -> String
     show (Canvas width height backgroundColor shapes) =
         "{ \"width\": "
             ++ show width
@@ -27,14 +31,24 @@ instance Show Canvas where
             ++ show shapes
             ++ " }"
 
+-- Outputs the canvas in JSON format, wrapped in a function call
 render :: Canvas -> IO ()
 render canvas = putStrLn $ "drawToCanvas(" ++ show canvas ++ ")"
 
+-- Creates a new canvas, setting the size of canvas element
 createCanvas :: Int -> Int -> Canvas
 createCanvas width height = Canvas width height Transparent []
 
-background :: Canvas -> Color -> Canvas
-background canvas color = canvas{backgroundColor = color}
+-- Set the background color of the canvas
+background :: Color -> Canvas -> Canvas
+background color canvas = canvas{backgroundColor = color}
 
-appendShape :: Canvas -> Shape -> Canvas
-appendShape canvas shape = canvas{shapes = shapes canvas ++ [shape]}
+-- Add a shape to the canvas
+(<<<) :: Canvas -> Shape -> Canvas
+(<<<) canvas shape = canvas{shapes = shapes canvas ++ [shape]}
+
+-- Individually modifies pixels in the canvas
+setPixel :: Canvas -> Vector -> Color -> Canvas
+setPixel canvas position color = canvas{shapes = shapes canvas ++ [pixel]}
+  where
+    pixel = fill color (stroke Transparent (translate position (square 1)))
