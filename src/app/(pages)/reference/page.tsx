ca2755878643df94e82dea29d404f62aa75e1223
Ollie Jones <oathompsonjones@gmail.com>
2025-01-23 13:32:06 +0000

    Add notation section to reference page and prevent line breaks inside code elements

--- src/app/(pages)/reference/page.tsx
+++ src/app/(pages)/reference/page.tsx
@@ -14,6 +14,35 @@ const list = (items: ReactNode[]): Awaited<ReactNode> => <ul>{items.map((item, i
 
 const docs: Record<string, Section> = {
     /* eslint-disable @typescript-eslint/naming-convention, sort-keys */
+    Notation: () => (
+        <div>
+            Please note that the following documentation makes use of Haskell type signatures to describe functions.
+            <br />
+            Haskell type signatures look like this: <code>identifier :: Type1 -&gt; Type2 -&gt; ... -&gt; TypeN</code>.
+            <br />
+            The <code>::</code> symbol is read as "has type", and the <code>-&gt;</code> symbol is read as "to".
+            <br />
+            The <code>identifier</code> is the name of the function, and the <code>TypeX</code> values are the types of
+            the arguments and the return value. The last <code>TypeN</code> is always the return value.
+            <br />
+            If an argument's type looks like this: <code>(Type1 -&gt; Type2)</code>, it means that that argument is a
+            function that takes a <code>Type1</code> and returns a <code>Type2</code>.
+            <br />
+            Infix operators in Haskell are defined the same way as functions, but with the operator name in parentheses.
+            <Typography variant="h4">Examples</Typography>
+            <code>add :: Int -&gt; Int -&gt; Int</code> — A function that takes two integers and returns an integer (
+            <code>add 6 7</code> = <code>13</code>).
+            <br />
+            <code>(+) :: Int -&gt; Int -&gt; Int</code> — The addition operator, which takes two integers and returns an
+                integer (<code>6 + 7</code> = <code>13</code>).
+            <br />
+            <br />
+            <code>map :: (a -&gt; b) -&gt; [a] -&gt; [b]</code> — A function that takes another function, which
+                converts from type <code>a</code> to type <code>b</code>, and a list of <code>a</code>s and returns a
+                list of <code>b</code>s, by applying the given function to each element of the list (<code>
+                map (add 5) [1, 2, 3, 4, 5]</code> = <code>[6, 7, 8, 9, 10]</code>).
+        </div>
+    ),
     "Editor Controls": (metaKey: ReactNode) => (
         <div>
             <Grid2 container alignItems="center">
