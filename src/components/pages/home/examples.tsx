d042ec9e202cf01bd9c82360a2224de6d2c9c265
Ollie Jones <oathompsonjones@gmail.com>
2025-03-15 01:44:47 +0000

    A couple of CSS fixes/alternatives because Firefox sucks

--- src/components/pages/home/examples.tsx
+++ src/components/pages/home/examples.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { Example } from "./example";
 import type { ReactNode } from "react";
 import type { StaticImageData } from "next/image";
 import styles from "styles/components/pages/home/examples.module.css";
+import { useEffect } from "react";
 
 /**
  * Render example sketches.
@@ -12,6 +15,14 @@ import styles from "styles/components/pages/home/examples.module.css";
 export function Examples({ examples }: {
     examples: Array<{ code: string; image: StaticImageData; name: string; }>;
 }): ReactNode {
+    // Safari and Chrome can do this by just applying `justify-content: center;` to the container.
+    // Firefox needs a little help.
+    useEffect(() => {
+        const containers = document.querySelectorAll(`.${styles.examplesScroll}`);
+        for (const container of containers)
+            container.scrollLeft = (container.scrollWidth - container.clientWidth) / 2;
+    }, []);
+
     return (
         <div className={styles.examplesContainer}>
             <div className={styles.examplesScroll}>
